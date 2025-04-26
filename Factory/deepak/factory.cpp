6cced32109dc34521f13b46f1cb4bb546c62493d
ishuBelwal <ishubelwal@gmail.com>
2025-04-26 13:40:29 +0530

    :fire: Remove Duplicate File

--- Factory/deepak/factory.cpp
+++ Factory/deepak/factory.cpp
@@ -1,7 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bis/tdc++.h>
+using namespace std
 
-class Product {
+    class Product {
 public:
   virtual ~Product() {};
   virtual void Operation() {};
